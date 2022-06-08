4ff0fb3129a20837336dd92637136025ddbeb650
Ronald Covington <rpcovington2@gmail.com>
2022-06-07 21:27:10 -0400

    changed app to apps

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:CreateApp --log-file=-
+web: gunicorn app:apps --log-file=-
--- app.py
+++ app.py
@@ -2,5 +2,5 @@ from web import CreateApp
 
 if __name__ == '__main__':
 
-    app = CreateApp()
-    app.run()
+    apps = CreateApp()
+    apps.run()
