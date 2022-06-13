ac569f4c3436fde1453c7a68818c6a688109662e
Ronald Covington <rpcovington2@gmail.com>
2022-06-13 14:41:31 -0400

    Formatting Changes
    Add a prtice.html for quoting new items and added shop pages for listing items

--- app.py
+++
@@ -1,6 +0,0 @@
-from web import CreateApp, app
-
-if __name__ == '__main__':
-
-    app1 = CreateApp()
-    app1.run()
--- web/views.py
+++ web/views.py
@@ -1,11 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for, redirect
 
 views = Blueprint('views', __name__)
 
 
 @views.route("/")
 def home():
-    return render_template("Home.html")
+    return render_template("index.html")
 
 
 @views.route("/profile")
@@ -22,6 +22,23 @@ def events():
 def projects():
     return render_template("Projects.html")
 
+
 @views.route("/shop")
 def Shop():
     return render_template("Shop.html")
+
+
+@views.route("/item")
+def item():
+    return render_template("item.html")
+
+
+@views.route("/shop/<item>")
+def id(item):
+    print(item)
+    return render_template("item.html")
+
+@views.route("/pricing")
+def princing():
+
+    return render_template("price.html")
