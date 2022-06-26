acd9df2a1fe2141e8f8591f5d0a9c6b12ba837bf
Ronald Covington <rpcovington2@gmail.com>
2022-06-26 10:29:26 -0400

    Added Profile data

--- web/models.py
+++ web/models.py
@@ -20,6 +20,18 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Submit')
 
 
+class EditProfile(FlaskForm):
+    FirstName = StringField('First Name')
+    LastName = StringField('Last Name')
+    Email = StringField('E-Mail Address')
+    HomePhone = StringField('Phone')
+    MobilePhone = StringField('Mobile')
+    Address = StringField('Address')
+    City = StringField('City')
+    State = StringField('State')
+    submit = SubmitField('Submit')
+
+
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
@@ -28,3 +40,25 @@ class User(db.Model, UserMixin):
     LastName = db.Column(db.String(80))
     Email = db.Column(db.String(255))
     PasswordHash = db.Column(db.String(45))
+
+
+class Info(db.Model):
+    __tablename__ = 'userinfo'
+    id = db.Column(db.Integer, primary_key=True)
+    FirstName = db.Column(db.String(80))
+    LastName = db.Column(db.String(80))
+    Email = db.Column(db.String(255))
+    Phone = db.Column(db.String(80))
+    Mobile = db.Column(db.String(80))
+    Address = db.Column(db.String(80))
+    City = db.Column(db.String(80))
+    State = db.Column(db.String(80))
+    Title = db.Column(db.String(80))
+
+
+class Projects(db.Model):
+    __tablename__ = 'projects'
+    id = db.Column(db.Integer, primary_key=True)
+    Name = db.Column(db.String(80))
+    StartDate = db.Column(db.String(80))
+    EndDate = db.Column(db.String(255))
--- web/views.py
+++ web/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, url_for, redirect, flash
 from flask_login import login_user, login_required, logout_user, current_user
-from web.models import SignUpForm, User, LoginForm
+from web.models import *
 from werkzeug.security import generate_password_hash, check_password_hash
 
 views = Blueprint('views', __name__)
@@ -28,42 +28,166 @@ def home():
 @views.route("/profile")
 @login_required
 def profile():
-    return render_template("Profile.html", user=current_user)
+
+    info = Info.query.filter_by(Email=current_user.Email).first()
+
+    EditProfileForm = EditProfile()
+
+    if EditProfileForm.validate_on_submit():
+        print(EditProfileForm.FirstName.data)
+        print(EditProfileForm.LastName.data)
+        print(EditProfileForm.MobilePhone.data)
+        print(EditProfileForm.Address.data)
+        print(EditProfileForm.City.data)
+        print(EditProfileForm.State.data)
+    return render_template("Profile.html", users=info, user=current_user, ProfileForm=EditProfileForm)
 
 
 @views.route("/events")
 def events():
-    return render_template("Events.html", user=current_user)
+    loginForm = LoginForm()
+
+    if loginForm.validate_on_submit():
+        email = loginForm.Email.data
+        user = User.query.filter_by(Email=email).first()
+        if user:
+            if check_password_hash(user.PasswordHash, loginForm.Password.data):
+                login_user(user, remember=True)
+                flash("Login Successful!!", 'Success')
+                return redirect(url_for('views.home'))
+        else:
+            print("Error")
+            flash("Sign up", 'error')
+
+    return render_template("Events.html", user=current_user, loginform=loginForm)
 
 
 @views.route("/projects")
+@login_required
 def projects():
-    return render_template("Projects.html", user=current_user)
+    loginForm = LoginForm()
+
+    if loginForm.validate_on_submit():
+        email = loginForm.Email.data
+        user = User.query.filter_by(Email=email).first()
+        if user:
+            if check_password_hash(user.PasswordHash, loginForm.Password.data):
+                login_user(user, remember=True)
+                flash("Login Successful!!", 'Success')
+                return redirect(url_for('views.home'))
+        else:
+            print("Error")
+            flash("Sign up", 'error')
+
+
+
+    return render_template("Projects.html", user=current_user, loginform=loginForm)
 
 
 @views.route("/shop")
 def Shop():
-    return render_template("Shop.html", user=current_user)
+    loginForm = LoginForm()
+
+    if loginForm.validate_on_submit():
+        email = loginForm.Email.data
+        user = User.query.filter_by(Email=email).first()
+        if user:
+            if check_password_hash(user.PasswordHash, loginForm.Password.data):
+                login_user(user, remember=True)
+                flash("Login Successful!!", 'Success')
+                return redirect(url_for('views.home'))
+        else:
+            print("Error")
+            flash("Sign up", 'error')
+    return render_template("Shop.html", user=current_user, loginform=loginForm)
 
 
 @views.route("/item")
 def item():
-    return render_template("item.html")
+    loginForm = LoginForm()
+
+    if loginForm.validate_on_submit():
+        email = loginForm.Email.data
+        user = User.query.filter_by(Email=email).first()
+        if user:
+            if check_password_hash(user.PasswordHash, loginForm.Password.data):
+                login_user(user, remember=True)
+                flash("Login Successful!!", 'Success')
+                return redirect(url_for('views.home'))
+        else:
+            print("Error")
+            flash("Sign up", 'error')
+    return render_template("item.html", user=current_user, loginform=loginForm)
 
 
 @views.route("/shop/<item>")
 def id(item):
-    print(item)
-    return render_template("item.html")
+    loginForm = LoginForm()
+
+    if loginForm.validate_on_submit():
+        email = loginForm.Email.data
+        user = User.query.filter_by(Email=email).first()
+        if user:
+            if check_password_hash(user.PasswordHash, loginForm.Password.data):
+                login_user(user, remember=True)
+                flash("Login Successful!!", 'Success')
+                return redirect(url_for('views.home'))
+        else:
+            print("Error")
+            flash("Sign up", 'error')
+    return render_template("item.html", user=current_user, loginform=loginForm)
 
 
 @views.route("/pricing")
 def pricing():
+    loginForm = LoginForm()
 
-    return render_template("price.html")
+    if loginForm.validate_on_submit():
+        email = loginForm.Email.data
+        user = User.query.filter_by(Email=email).first()
+        if user:
+            if check_password_hash(user.PasswordHash, loginForm.Password.data):
+                login_user(user, remember=True)
+                flash("Login Successful!!", 'Success')
+                return redirect(url_for('views.home'))
+        else:
+            print("Error")
+            flash("Sign up", 'error')
+    return render_template("price.html", user=current_user, loginform=loginForm)
 
 
 @views.route("/learn")
 def learn():
+    loginForm = LoginForm()
 
-    return render_template("learn.html")
+    if loginForm.validate_on_submit():
+        email = loginForm.Email.data
+        user = User.query.filter_by(Email=email).first()
+        if user:
+            if check_password_hash(user.PasswordHash, loginForm.Password.data):
+                login_user(user, remember=True)
+                flash("Login Successful!!", 'Success')
+                return redirect(url_for('views.home'))
+        else:
+            print("Error")
+            flash("Sign up", 'error')
+
+    return render_template("learn.html", user=current_user, loginform=loginForm)
+
+
+def Login():
+    loginForm = LoginForm()
+
+    if loginForm.validate_on_submit():
+        email = loginForm.Email.data
+        user = User.query.filter_by(Email=email).first()
+        if user:
+            if check_password_hash(user.PasswordHash, loginForm.Password.data):
+                login_user(user, remember=True)
+                flash("Login Successful!!", 'Success')
+                return redirect(url_for('views.home'))
+        else:
+            print("Error")
+            flash("Sign up", 'error')
+
+    return loginForm
